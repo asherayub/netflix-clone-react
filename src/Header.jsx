f2de78aa0b2c0683892cfa28a450638d777d2bfb
asherayub <asherayub52@gmail.com>
2023-04-17 09:44:44 +0500

    logo messed up

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img className="logo" src="./assets/Netflix_Logo_PMS.png" alt="Netflix Logo" />
+      <img className="logo" src="../Netflix_Logo_PMS.png" alt="Netflix Logo" />
     </header>
   );
 };
